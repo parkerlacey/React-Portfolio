e842f2bb9beb7065a644366c0d6f9dcca2ccc3e3
Parker Lacey <parkerlacey2424@gmail.com>
2023-01-24 18:21:45 -0600

    imported projects

---
+++ src/helpers/ProjectList.js
@@ -0,0 +1,39 @@
+import Proj1 from "../assets/weatherdashboard.png";
+import Proj2 from "../assets/movienight.png";
+import Proj3 from "../assets/codequiz.png";
+import Proj4 from "../assets/password.png";
+import Proj5 from "../assets/proj5.jpg";
+import Proj6 from "../assets/proj6.png";
+
+export const ProjectList = [
+  {
+    name: "Weather Dashboard",
+    image: Proj1,
+    skills: "JavaScript,HTML,CSS",
+  },
+  {
+    name: "Movie Night",
+    image: Proj2,
+    skills: "HTML, CSS, Javascript, BootStrapJS, OMDB API",
+  },
+  {
+    name: "Code Quiz",
+    image: Proj3,
+    skills: "React, Node.js, HTML, CSS, Javascript",
+  },
+  {
+    name: "Password Generator",
+    image: Proj4,
+    skills: "React,Node.js,MySQL,GraphQL",
+  },
+  {
+    name: "Dashboard Visualizer",
+    image: Proj5,
+    skills: "JavaScript,HTML,CSS",
+  },
+  {
+    name: "Mobile Game",
+    image: Proj6,
+    skills: "React Native,JavaScript,HTML,CSS",
+  },
+];
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,19 +1,22 @@
-import React from 'react'
-import ProjectItem from "../components/ProjectItem"
-import Proj1 from '../assets/proj1.jpg'
-import Proj3 from '../assets/proj3.webp'
-import '../styles/Projects.css'
+import React from "react";
+import ProjectItem from "../components/ProjectItem";
+import { ProjectList } from "../helpers/ProjectList";
+
+import "../styles/Projects.css";
 
 function Projects() {
   return (
-    <div className='projects'>
-      <h1>My Personal Projects</h1>
-      <div className='projecList'>
-        <ProjectItem name="Social Media Website" image={Proj1}/>
-        <ProjectItem name="Spotify Clone" image={Proj3} />
+    <div className="projects">
+      <h1> My Personal Projects</h1>
+      <div className="projectList">
+        {ProjectList.map((project, idx) => {
+          return (
+            <ProjectItem id={idx} name={project.name} image={project.image} />
+          );
+        })}
       </div>
     </div>
-  )
+  );
 }
 
 export default Projects;
